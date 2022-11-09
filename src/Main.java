4ee0ee7500208eb27bd2b52de816ba8ac62b46ec
Andrey Piterkin <andrey.piterkin@gmail.com>
2022-11-08 22:50:11 -0500

    Added support for PNG

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import javax.imageio.ImageIO;
 
 import cs3500.imageprocessor.model.BasicPPMImageEditor;
+import cs3500.imageprocessor.model.IPixel;
 import cs3500.imageprocessor.model.ImageBMP;
 import cs3500.imageprocessor.model.ImageEditor;
 import cs3500.imageprocessor.model.ImageJPG;
@@ -34,8 +35,19 @@ public class Main {
   public static void main(String[] args) throws IOException {
     ImageEditor model = new MultiformatImageEditor();
     ImageEditorView view = new ImageEditorViewStub();
-    model.importImageFromDisk("res/me.png", "cat");
-    ImageState img = model.getImage("cat");
-    new ImageBMP(img).save("res/me2.bmp");
+    model.importImageFromDisk("res/me2.ppm", "me");
+    model.importImageFromDisk("res/me2.bmp", "me2");
+    ImageState i1 = model.getImage("me");
+    ImageState i2 = model.getImage("me2");
+
+    for (int r = 0; r < i1.getHeight(); r++) {
+      for (int c = 0; c < i1.getWidth(); c++) {
+        IPixel p = i1.getPixelAt(r, c);
+        IPixel p2 = i2.getPixelAt(r, c);
+        if (p.getRed() != p2.getRed() || p.getGreen() != p2.getGreen() || p.getBlue() != p2.getBlue()) {
+          System.out.println("Different at " + r + ", " + c);
+        }
+      }
+    }
   }
 }
