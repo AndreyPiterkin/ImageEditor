e76e66474200f95267b2a45cc6ed3a9b1613b6cb
rohanchotirmall <rohanc2021@outlook.com>
2022-11-10 17:50:46 -0500

    Test

--- test/BasicImageTest.java
+++ test/BasicImageTest.java
@@ -1,160 +1,184 @@
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import cs3500.imageprocessor.model.BasicImage;
-//import cs3500.imageprocessor.model.RGBAPixel;
-//import cs3500.imageprocessor.model.ImageState;
-//import cs3500.imageprocessor.model.ImageUtil;
-//import cs3500.imageprocessor.operations.DarkenPixel;
-//import cs3500.imageprocessor.operations.FlipHorizontal;
-//import cs3500.imageprocessor.operations.GrayscaleRed;
-//import cs3500.imageprocessor.operations.VisualizeValue;
-//
-//import static org.junit.Assert.assertArrayEquals;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertThrows;
-//
-///**
-// * Tests for BasicImage.
-// */
-//public class BasicImageTest {
-//  BasicImage image1x1;
-//  BasicImage image2x2;
-//  BasicImage image3x3;
-//  BasicImage image3x3Copy;
-//  RGBAPixel[][] initPixels1 = {{new RGBAPixel(0, 128, 240)}};
-//  RGBAPixel[][] initPixels2 = {
-//      {new RGBAPixel(0, 128, 240),
-//          new RGBAPixel(1, 129, 241)},
-//      {new RGBAPixel(2, 130, 242),
-//          new RGBAPixel(3, 131, 243)}};
-//  RGBAPixel[][] initPixels3 = {
-//      {new RGBAPixel(0, 128, 240),
-//          new RGBAPixel(1, 129, 241),
-//          new RGBAPixel(0, 128, 240)},
-//      {new RGBAPixel(2, 130, 242),
-//          new RGBAPixel(3, 131, 243),
-//          new RGBAPixel(2, 130, 242)},
-//      {new RGBAPixel(2, 130, 242),
-//          new RGBAPixel(3, 131, 243),
-//          new RGBAPixel(2, 130, 242)}};
-//
-//
-//  @Before
-//  public void initData() {
-//
-//    this.image1x1 = new ImagePPM(this.initPixels1);
-//    this.image2x2 = new ImagePPM(this.initPixels2);
-//    this.image3x3 = new ImagePPM(this.initPixels3);
-//    this.image3x3Copy = new ImagePPM(this.image3x3);
-//  }
-//
-//  @Test
-//  public void testInvalidConstructors() {
-//    RGBAPixel[][] nullPixels = null;
-//    assertThrows(IllegalArgumentException.class, () -> this.image1x1 = new ImagePPM(nullPixels));
-//    RGBAPixel[][] emptyPixels = {};
-//    assertThrows(IllegalArgumentException.class, () -> this.image1x1 = new ImagePPM(emptyPixels));
-//    RGBAPixel[][] nullOuterPixels = {null};
-//    assertThrows(NullPointerException.class, () -> this.image1x1 = new ImagePPM(nullOuterPixels));
-//    RGBAPixel[][] nullInnerPixels = {{null}};
-//    assertThrows(NullPointerException.class, () -> this.image1x1 = new ImagePPM(nullInnerPixels));
-//  }
-//
-//  @Test
-//  public void testGetWidth() {
-//    assertEquals(1, this.image1x1.getWidth());
-//    assertEquals(2, this.image2x2.getWidth());
-//    assertEquals(3, this.image3x3.getWidth());
-//    assertEquals(3, this.image3x3Copy.getWidth());
-//
-//  }
-//
-//  @Test
-//  public void testGetHeight() {
-//    assertEquals(1, this.image1x1.getHeight());
-//    assertEquals(2, this.image2x2.getHeight());
-//    assertEquals(3, this.image3x3.getHeight());
-//    assertEquals(3, this.image3x3Copy.getHeight());
-//  }
-//
-//  @Test
-//  public void testGetPixelAt() {
-//    for (int i = 0; i < this.image1x1.getHeight(); i++) {
-//      for (int j = 0; j < this.image1x1.getWidth(); j++) {
-//        assertEquals(this.initPixels1[i][j], this.image1x1.getPixelAt(i, j));
-//      }
-//    }
-//    for (int i = 0; i < this.image2x2.getHeight(); i++) {
-//      for (int j = 0; j < this.image2x2.getWidth(); j++) {
-//        assertEquals(this.initPixels2[i][j], this.image2x2.getPixelAt(i, j));
-//      }
-//    }
-//    for (int i = 0; i < this.image3x3.getHeight(); i++) {
-//      for (int j = 0; j < this.image3x3.getWidth(); j++) {
-//        assertEquals(this.initPixels3[i][j], this.image3x3.getPixelAt(i, j));
-//      }
-//    }
-//    for (int i = 0; i < this.image3x3Copy.getHeight(); i++) {
-//      for (int j = 0; j < this.image3x3Copy.getWidth(); j++) {
-//        assertEquals(this.initPixels3[i][j], this.image3x3Copy.getPixelAt(i, j));
-//      }
-//    }
-//  }
-//
-//  @Test
-//  public void testApply() {
-//    ImageState darkened = this.image2x2.apply(new DarkenPixel(10));
-//    RGBAPixel[][] darkenedPixels = {
-//        {new RGBAPixel(0, 118, 230),
-//            new RGBAPixel(0, 119, 231)},
-//        {new RGBAPixel(0, 120, 232),
-//            new RGBAPixel(0, 121, 233)}};
-//
-//    ImageState flipHorz = this.image2x2.apply(new FlipHorizontal());
-//    RGBAPixel[][] flipPixels = {
-//        {new RGBAPixel(1, 129, 241),
-//            new RGBAPixel(0, 128, 240)},
-//        {new RGBAPixel(3, 131, 243),
-//            new RGBAPixel(2, 130, 242)}};
-//
-//    ImageState grayscaleRed = this.image2x2.apply(new GrayscaleRed());
-//    RGBAPixel[][] red = {
-//        {new RGBAPixel(0, 0, 0),
-//            new RGBAPixel(1, 1, 1)},
-//        {new RGBAPixel(2, 2, 2),
-//            new RGBAPixel(3, 3, 3)}};
-//
-//    ImageState valueImage = this.image2x2.apply(new VisualizeValue());
-//    RGBAPixel[][] value = {
-//        {new RGBAPixel(240, 240, 240),
-//            new RGBAPixel(241, 241, 241)},
-//        {new RGBAPixel(242, 242, 242),
-//            new RGBAPixel(243, 243, 243)}};
-//
-//    for (int i = 0; i < 2; i++) {
-//      for (int j = 0; j < 2; j++) {
-//        assertEquals(darkenedPixels[i][j], darkened.getPixelAt(i, j));
-//        assertEquals(flipPixels[i][j], flipHorz.getPixelAt(i, j));
-//        assertEquals(red[i][j], grayscaleRed.getPixelAt(i, j));
-//        assertEquals(value[i][j], valueImage.getPixelAt(i, j));
-//      }
-//    }
-//  }
-//
-//  @Test
-//  public void testSave() {
-//    ImageUtil util = new ImageUtil();
-//    this.image1x1.save("images/testReadPPM2.ppm");
-//    RGBAPixel[][] readPixels1 = util.readPPM("images/testReadPPM2.ppm");
-//    assertArrayEquals(this.initPixels1, readPixels1);
-//
-//    this.image2x2.save("images/testReadPPM1.ppm");
-//    RGBAPixel[][] readPixels2 = util.readPPM("images/testReadPPM1.ppm");
-//    assertArrayEquals(this.initPixels2, readPixels2);
-//
-//    this.image3x3.save("images/testReadPPM3.ppm");
-//    RGBAPixel[][] readPixels3 = util.readPPM("images/testReadPPM3.ppm");
-//    assertArrayEquals(this.initPixels3, readPixels3);
-//  }
-//}
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import cs3500.imageprocessor.model.BasicImage;
+import cs3500.imageprocessor.model.ImageState;
+import cs3500.imageprocessor.model.RGBAPixel;
+import cs3500.imageprocessor.operations.DarkenPixel;
+import cs3500.imageprocessor.operations.FlipHorizontal;
+import cs3500.imageprocessor.operations.GrayscaleRed;
+import cs3500.imageprocessor.operations.VisualizeValue;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for BasicImage.
+ */
+public class BasicImageTest {
+  BasicImage image1x1;
+  BasicImage image2x2;
+  BasicImage image3x3;
+  BufferedImage img1;
+  BufferedImage img2;
+  BufferedImage img3;
+  RGBAPixel[][] initPixels1 = {{new RGBAPixel(0, 128, 240)}};
+  RGBAPixel[][] initPixels2 = {
+      {new RGBAPixel(0, 128, 240),
+          new RGBAPixel(1, 129, 241)},
+      {new RGBAPixel(2, 130, 242),
+          new RGBAPixel(3, 131, 243)}};
+  RGBAPixel[][] initPixels3 = {
+      {new RGBAPixel(0, 128, 240),
+          new RGBAPixel(1, 129, 241),
+          new RGBAPixel(0, 128, 240)},
+      {new RGBAPixel(2, 130, 242),
+          new RGBAPixel(3, 131, 243),
+          new RGBAPixel(2, 130, 242)},
+      {new RGBAPixel(2, 130, 242),
+          new RGBAPixel(3, 131, 243),
+          new RGBAPixel(2, 130, 242)}};
+
+  @Before
+  public void initData() {
+    this.img1 = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
+    for (int r = 0; r < initPixels1.length; r++) {
+      for (int c = 0; c < initPixels1[r].length; c++) {
+        RGBAPixel pixel = initPixels1[r][c];
+        Color color = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue());
+        img1.setRGB(c, r, color.getRGB());
+      }
+    }
+
+    this.img2 = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
+    for (int r = 0; r < initPixels2.length; r++) {
+      for (int c = 0; c < initPixels2[r].length; c++) {
+        RGBAPixel pixel = initPixels2[r][c];
+        Color color = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue());
+        img2.setRGB(c, r, color.getRGB());
+      }
+    }
+
+    this.img3 = new BufferedImage(3, 3, BufferedImage.TYPE_INT_RGB);
+    for (int r = 0; r < initPixels3.length; r++) {
+      for (int c = 0; c < initPixels3[r].length; c++) {
+        RGBAPixel pixel = initPixels3[r][c];
+        Color color = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue());
+        img3.setRGB(c, r, color.getRGB());
+      }
+    }
+
+    this.image1x1 = new BasicImage(img1);
+    this.image2x2 = new BasicImage(img2);
+    this.image3x3 = new BasicImage(img3);
+  }
+
+  @Test
+  public void testGetWidth() {
+    assertEquals(1, this.image1x1.getWidth());
+    assertEquals(2, this.image2x2.getWidth());
+    assertEquals(3, this.image3x3.getWidth());
+  }
+
+  @Test
+  public void testGetHeight() {
+    assertEquals(1, this.image1x1.getHeight());
+    assertEquals(2, this.image2x2.getHeight());
+    assertEquals(3, this.image3x3.getHeight());
+  }
+
+  @Test
+  public void testGetPixelAt() {
+    for (int i = 0; i < this.image1x1.getHeight(); i++) {
+      for (int j = 0; j < this.image1x1.getWidth(); j++) {
+        assertEquals(this.initPixels1[i][j], this.image1x1.getPixelAt(i, j));
+      }
+    }
+    for (int i = 0; i < this.image2x2.getHeight(); i++) {
+      for (int j = 0; j < this.image2x2.getWidth(); j++) {
+        assertEquals(this.initPixels2[i][j], this.image2x2.getPixelAt(i, j));
+      }
+    }
+    for (int i = 0; i < this.image3x3.getHeight(); i++) {
+      for (int j = 0; j < this.image3x3.getWidth(); j++) {
+        assertEquals(this.initPixels3[i][j], this.image3x3.getPixelAt(i, j));
+      }
+    }
+  }
+
+  @Test
+  public void testApply() {
+    ImageState darkened = this.image2x2.apply(new DarkenPixel(10));
+    RGBAPixel[][] darkenedPixels = {
+        {new RGBAPixel(0, 118, 230),
+            new RGBAPixel(0, 119, 231)},
+        {new RGBAPixel(0, 120, 232),
+            new RGBAPixel(0, 121, 233)}};
+
+    ImageState flipHorz = this.image2x2.apply(new FlipHorizontal());
+    RGBAPixel[][] flipPixels = {
+        {new RGBAPixel(1, 129, 241),
+            new RGBAPixel(0, 128, 240)},
+        {new RGBAPixel(3, 131, 243),
+            new RGBAPixel(2, 130, 242)}};
+
+    ImageState grayscaleRed = this.image2x2.apply(new GrayscaleRed());
+    RGBAPixel[][] red = {
+        {new RGBAPixel(0, 0, 0),
+            new RGBAPixel(1, 1, 1)},
+        {new RGBAPixel(2, 2, 2),
+            new RGBAPixel(3, 3, 3)}};
+
+    ImageState valueImage = this.image2x2.apply(new VisualizeValue());
+    RGBAPixel[][] value = {
+        {new RGBAPixel(240, 240, 240),
+            new RGBAPixel(241, 241, 241)},
+        {new RGBAPixel(242, 242, 242),
+            new RGBAPixel(243, 243, 243)}};
+
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 2; j++) {
+        assertEquals(darkenedPixels[i][j], darkened.getPixelAt(i, j));
+        assertEquals(flipPixels[i][j], flipHorz.getPixelAt(i, j));
+        assertEquals(red[i][j], grayscaleRed.getPixelAt(i, j));
+        assertEquals(value[i][j], valueImage.getPixelAt(i, j));
+      }
+    }
+  }
+
+  @Test
+  public void asBufferedImageTest() {
+    assertEquals(this.img1.getRGB(0, 0),
+        image1x1.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(0, 0));
+
+    assertEquals(this.img2.getRGB(0, 0),
+        image2x2.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(0, 0));
+    assertEquals(this.img2.getRGB(0, 1),
+        image2x2.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(0, 1));
+    assertEquals(this.img2.getRGB(1, 0),
+        image2x2.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(1, 0));
+    assertEquals(this.img2.getRGB(1, 1),
+        image2x2.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(1, 1));
+
+    assertEquals(this.img3.getRGB(0, 0),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(0, 0));
+    assertEquals(this.img3.getRGB(0, 1),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(0, 1));
+    assertEquals(this.img3.getRGB(0, 2),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(0, 2));
+    assertEquals(this.img3.getRGB(1, 0),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(1, 0));
+    assertEquals(this.img3.getRGB(1, 1),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(1, 1));
+    assertEquals(this.img3.getRGB(1, 2),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(1, 2));
+    assertEquals(this.img3.getRGB(2, 0),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(2, 0));
+    assertEquals(this.img3.getRGB(2, 1),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(2, 1));
+    assertEquals(this.img3.getRGB(2, 1),
+        image3x3.asBufferedImage(BufferedImage.TYPE_INT_RGB).getRGB(2, 2));
+  }
+}
--- test/ColorOperationTest.java
+++ test/ColorOperationTest.java
@@ -1,5 +1,114 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+import cs3500.imageprocessor.model.BasicImage;
+import cs3500.imageprocessor.model.ImageState;
+import cs3500.imageprocessor.model.RGBAPixel;
+import cs3500.imageprocessor.operations.ColorOperation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 /**
  * Tests for ColorOperation.
  */
 public class ColorOperationTest {
+  private ColorOperation colorOperation;
+  private ColorOperation colorOperation2;
+  private ImageState image2x2;
+  private ImageState image3x3;
+
+  @Before
+  public void initData() {
+    double[][] kernel1 = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+    double[][] kernel2 = {{2.5, 2.5, 2.5}, {2.5, 2.5, 2.5}, {2.5, 2.5, 2.5}};
+    this.colorOperation = new ColorOperation(kernel1);
+    this.colorOperation2 = new ColorOperation(kernel2);
+
+    RGBAPixel[][] initPixels2x2 = {
+        {new RGBAPixel(1, 2, 3),
+            new RGBAPixel(4, 5, 6)},
+        {new RGBAPixel(7, 8, 9),
+            new RGBAPixel(10, 11, 12)}};
+    RGBAPixel[][] initPixels3x3 = {
+        {new RGBAPixel(1, 2, 3),
+            new RGBAPixel(3, 4, 5),
+            new RGBAPixel(5, 6, 7)},
+        {new RGBAPixel(7, 8, 9),
+            new RGBAPixel(9, 10, 11),
+            new RGBAPixel(11, 12, 13)},
+        {new RGBAPixel(13, 14, 15),
+            new RGBAPixel(15, 16, 17),
+            new RGBAPixel(17, 18, 19)}};
+
+    BufferedImage img1 = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
+    for (int r = 0; r < initPixels2x2.length; r++) {
+      for (int c = 0; c < initPixels2x2[r].length; c++) {
+        RGBAPixel pixel = initPixels2x2[r][c];
+        Color color = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue());
+        img1.setRGB(c, r, color.getRGB());
+      }
+    }
+
+    BufferedImage img2 = new BufferedImage(3, 3, BufferedImage.TYPE_INT_RGB);
+    for (int r = 0; r < initPixels3x3.length; r++) {
+      for (int c = 0; c < initPixels3x3[r].length; c++) {
+        RGBAPixel pixel = initPixels3x3[r][c];
+        Color color = new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue());
+        img2.setRGB(c, r, color.getRGB());
+      }
+    }
+
+    this.image2x2 = new BasicImage(img1);
+    this.image3x3 = new BasicImage(img2);
+  }
+
+  @Test
+  public void testConstructorExns() {
+    double[][] kernel1x3 = {{1, 2, 3}};
+    double[][] kernel3x1 = {{1}, {2}, {3}};
+    double[][] kernel1x1 = {{1}};
+    assertThrows(IllegalArgumentException.class, () -> this.colorOperation =
+        new ColorOperation(kernel1x3));
+    assertThrows(IllegalArgumentException.class, () -> this.colorOperation =
+        new ColorOperation(kernel3x1));
+    assertThrows(IllegalArgumentException.class, () -> this.colorOperation =
+        new ColorOperation(kernel1x1));
+  }
+
+  @Test
+  public void testApply() {
+    assertEquals(new RGBAPixel(6), this.colorOperation.apply(this.image2x2, 0, 0));
+    assertEquals(new RGBAPixel(15), this.colorOperation.apply(this.image2x2, 0, 1));
+    assertEquals(new RGBAPixel(24), this.colorOperation.apply(this.image2x2, 1, 0));
+    assertEquals(new RGBAPixel(33), this.colorOperation.apply(this.image2x2, 1, 1));
+
+    assertEquals(new RGBAPixel(6), this.colorOperation.apply(this.image3x3, 0, 0));
+    assertEquals(new RGBAPixel(12), this.colorOperation.apply(this.image3x3, 0, 1));
+    assertEquals(new RGBAPixel(18), this.colorOperation.apply(this.image3x3, 0, 2));
+    assertEquals(new RGBAPixel(24), this.colorOperation.apply(this.image3x3, 1, 0));
+    assertEquals(new RGBAPixel(30), this.colorOperation.apply(this.image3x3, 1, 1));
+    assertEquals(new RGBAPixel(36), this.colorOperation.apply(this.image3x3, 1, 2));
+    assertEquals(new RGBAPixel(42), this.colorOperation.apply(this.image3x3, 2, 0));
+    assertEquals(new RGBAPixel(48), this.colorOperation.apply(this.image3x3, 2, 1));
+    assertEquals(new RGBAPixel(54), this.colorOperation.apply(this.image3x3, 2, 2));
+
+    assertEquals(new RGBAPixel(15), this.colorOperation2.apply(this.image2x2, 0, 0));
+    assertEquals(new RGBAPixel(37), this.colorOperation2.apply(this.image2x2, 0, 1));
+    assertEquals(new RGBAPixel(60), this.colorOperation2.apply(this.image2x2, 1, 0));
+    assertEquals(new RGBAPixel(82), this.colorOperation2.apply(this.image2x2, 1, 1));
+
+    assertEquals(new RGBAPixel(15), this.colorOperation2.apply(this.image3x3, 0, 0));
+    assertEquals(new RGBAPixel(30), this.colorOperation2.apply(this.image3x3, 0, 1));
+    assertEquals(new RGBAPixel(45), this.colorOperation2.apply(this.image3x3, 0, 2));
+    assertEquals(new RGBAPixel(60), this.colorOperation2.apply(this.image3x3, 1, 0));
+    assertEquals(new RGBAPixel(75), this.colorOperation2.apply(this.image3x3, 1, 1));
+    assertEquals(new RGBAPixel(90), this.colorOperation2.apply(this.image3x3, 1, 2));
+    assertEquals(new RGBAPixel(105), this.colorOperation2.apply(this.image3x3, 2, 0));
+    assertEquals(new RGBAPixel(120), this.colorOperation2.apply(this.image3x3, 2, 1));
+    assertEquals(new RGBAPixel(135), this.colorOperation2.apply(this.image3x3, 2, 2));
+  }
 }
